b7cd925328ccd0611e3ac3ebb4c89b1170a7a2ce
TPJamesH <108443772+TPJamesH@users.noreply.github.com>
2025-03-14 18:24:50 +0700

    - Implemented "delete "panel

--- Flask/frontend/src/component/panel_delete/api/deleteJewelry.js
+++ Flask/frontend/src/component/panel_delete/api/deleteJewelry.js
@@ -1,12 +1,10 @@
 import sendHttpRequest from "../../../http_call/HttpRequest";
 import JewelryUrlConfig from "../../../service_url/JewelryUrlConfig";
 
-export default async function addNewJewelry(newJewelry) {
+export default async function deleteJewelry(token) {
     let response = await sendHttpRequest(
-        JewelryUrlConfig.JEWELRY_SERVICE_URL,
-        "POST",
-        JSON.stringify(newJewelry)
-    )
+        JewelryUrlConfig.JEWELRY_SERVICE_URL+`/${token}`,
+        "DELETE")
     return response
 }
 
