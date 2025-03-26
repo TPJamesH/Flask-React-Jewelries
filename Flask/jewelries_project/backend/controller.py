75d6c10167d0ea9307183598a315e547d02bcf94
TPJamesH <108443772+TPJamesH@users.noreply.github.com>
2025-03-26 12:01:11 +0700

    - Fixed Information exposure through an exception

--- Flask/jewelries_project/backend/controller.py
+++ Flask/jewelries_project/backend/controller.py
@@ -1,3 +1,5 @@
+from logging import log
+import traceback
 from flask import Blueprint, jsonify,request
 from service import JewelryService
 
@@ -10,30 +12,34 @@ class JewelryController:
             data = self.jewelry_service.get_by_id(id)    
             return jsonify(data)
         except Exception as e:
-            return jsonify({"error": str(e)}),500
+            log(traceback.format_exc())
+            return "An internal error has occurred!"
         
     def upload_file(self):
         #check if there's a file
         if 'file' not in request.files:
-            return jsonify({'error': 'No file'}),400
+            log(traceback.format_exc())
+            return "An internal error has occurred!"
         
         file = request.files['file']
         
         #check if a file is selected
         if file.filename == '':
-            return jsonify({'error': 'No selected file'}),400
+            log(traceback.format_exc())
+            return "An internal error has occurred!"
         
         #validation
         if self.jewelry_service.upload_file(file):
             return jsonify({'message': 'File successsfully uploaded', 'filename': file.filename}),200
-        return jsonify({'error': 'File not allowed'}),400
+        
     
     def getAll(self):
         try:
             data = self.jewelry_service.get_all()      
             return jsonify(data)
         except Exception as e:
-            return jsonify({"error": str(e)}),500
+            log(traceback.format_exc())
+            return "An internal error has occurred!"
         
     def add_jewelry(self):
         try:
@@ -42,14 +48,16 @@ class JewelryController:
             self.jewelry_service.add_jewelry(j_data)
             return jsonify({"message": "Jewelry created successfully"}),201
         except Exception as e:
-            return jsonify({"error": str(e)}),500
+            log(traceback.format_exc())
+            return "An internal error has occurred!"
         
     def delete_jewelry(self,id: str):
         try:
             self.jewelry_service.delete_jewelry(id)
             return jsonify({"message": "Jewelry deleted successfully"}),200
         except Exception as e:
-            return jsonify({"error": str(e)}),500
+            log(traceback.format_exc())
+            return "An internal error has occurred!"
 
     def search_jewelry(self,searchText: str):
         try:
@@ -57,14 +65,16 @@ class JewelryController:
             data = self.jewelry_service.query_by_searchText(searchText)
             return jsonify(data)
         except Exception as e:
-            return jsonify({"error": str(e)}),500
+            log(traceback.format_exc())
+            return "An internal error has occurred!"
 
     def pagination(self,key: int, limit: int):
         try:
             data= self.jewelry_service.pagination(key,limit)
             return jsonify(data)
         except Exception as e:
-            return jsonify({"error": str(e)}),500
+            log(traceback.format_exc())
+            return "An internal error has occurred!"
 
     def pagination_search(self,key: int, limit: int, searchText:str):
          
@@ -73,7 +83,10 @@ class JewelryController:
                   data = self.jewelry_service.pagination_search(key,limit,searchText)
                   return jsonify(data)
               except Exception as e:
-                  return jsonify([]),404
+                  log(traceback.format_exc())
+                  return "An internal error has occurred!"
+              
+              
     def count(self):
         return self.jewelry_service.countTotal()
     
