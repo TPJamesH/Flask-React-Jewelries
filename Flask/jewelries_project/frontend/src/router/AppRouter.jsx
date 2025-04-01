64a6a75c7c97498ebb6aeb902282b885b1d628d9
TPJamesH <108443772+TPJamesH@users.noreply.github.com>
2025-04-01 18:23:17 +0700

    - Prototyping app router

--- Flask/jewelries_project/frontend/src/router/AppRouter.jsx
+++ Flask/jewelries_project/frontend/src/router/AppRouter.jsx
@@ -1,4 +1,21 @@
-import React, {Suspense, useEffect, useState} from "react"
-import {Routes, Route, Navigate, useLocation} from "react-router-dom"
+import React, { Suspense, useEffect, useState } from "react"
+import { Routes, Route, Navigate, useLocation } from "react-router-dom"
 import Authentication from "../Pages/Authentication"
-import 
+import Home from "../Pages/Home"
+import ProductPage from "../Pages/ProductPage"
+
+const AppRouter = () => {
+    const location = useLocation()
+    const excludedRoutes = [] //will be updated later
+
+    return (
+        <><Suspense fallback={<Loader className="flex-grow p-4" speed="normal" content="Normal" />} />
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/product" element={<ProductPage />} />
+            </Routes>
+        </>
+    )
+}
+
+export default AppRouter;
